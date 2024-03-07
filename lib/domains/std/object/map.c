82b6e08aed3b0f625927c1df21a11c42e008cd47
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-03-07 22:33:50 +0100

    Fixed map to look okay.

--- lib/domains/std/object/map.c
+++ lib/domains/std/object/map.c
@@ -10,17 +10,17 @@ void setup()
    set_untouched_desc("A map of Lima Bean is pinned to the wall.");
    set_in_room_desc("A map of Lima Bean has been discarded here.");
    set_long("It has a crude map of Lima Bean written on it.");
-   set_text("              MORTAL AREA    ATTIC                                     " +
-            "                   |        /                                          " +
-            "                   |       /  QUIET ROOM                               " +
-            "     LAVA CAVE     |      /  /                                         " +
-            "              \    |     /  /       PLAINS                             " +
-            "               \   |   U/  /           |                               " +
-            "  ELEVATOR --  WIZARD HALL  ---- EXAMPLE ROOM -- CAR WASH              " +
-            "              /D   |                   |                               " +
-            "             /     |                   |                               " +
-            "GENERAL STORE      |          RELATIONS & TOYS  -- ENVIRONMENTAL ROOM  " +
-            "              MONSTER ROOM             |                               " +
-            "                                       |                               " +
-            "                                 CULT OF LIMA                          ");
+   set_text("              MORTAL AREA    ATTIC                                     \n" +
+            "                   |        /                                          \n" +
+            "                   |       /  QUIET ROOM                               \n" +
+            "     LAVA CAVE     |      /  /                                         \n" +
+           "              \\    |     /  /       PLAINS                             \n" +
+           "               \\   |   U/  /           |                               \n" +
+            "  ELEVATOR --  WIZARD HALL  ---- EXAMPLE ROOM -- CAR WASH              \n" +
+            "              /D   |                   |                               \n" +
+            "             /     |                   |                               \n" +
+            "GENERAL STORE      |          RELATIONS & TOYS  -- ENVIRONMENTAL ROOM  \n" +
+            "              MONSTER ROOM             |                               \n" +
+            "                                       |                               \n" +
+            "                                 CULT OF LIMA                          \n");
 }
