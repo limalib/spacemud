67fcc083c551f4f64205fce0324e9377af43c181
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-03-19 23:06:07 +0100

    Stop dead adventurer from decaying.

--- lib/domains/std/object/dead_adventurer.c
+++ lib/domains/std/object/dead_adventurer.c
@@ -2,17 +2,19 @@
 
 inherit CORPSE;
 
-void mudlib_setup()
-{
-   ::mudlib_setup("adventurer");
-}
-
 void setup()
 {
+   ::mudlib_setup("adventurer"); // Sets the ID in the corpse.
    set_proper_name(0);
    set_adj("adventurer's", "brave");
    set_objects(
        (["/domains/std/weapon/pac_sword":1,
            "/domains/std/armour/fullplate":1, "/domains/std/object/lantern":1, "/domains/std/object/backpack":1, ]));
    set_unique(1);
+}
+
+// Do not decay. STATE_D will ignore it for returning 0 here.
+int state_update()
+{
+   return 0;
 }
