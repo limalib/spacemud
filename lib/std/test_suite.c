1347bbc046768871470cf7dfe6029901d6b78f45
Tsathoggua of Nkai <5774045+tsathoqqua@users.noreply.github.com>
2024-03-27 16:03:30 +0100

    Update test_suite.c

--- lib/std/test_suite.c
+++ lib/std/test_suite.c
@@ -18,7 +18,7 @@
 //       add_test_true("Some test",(: my_test:));
 //    }
 //
-// If you use ``add_test_true()`` the function succeeds if it returns 1, and fails on 1.
+// If you use ``add_test_true()`` the function succeeds if it returns 1 (anything that evaluates to true), and fails on 0.
 // If you use ``add_test_fail()``, quite the opposite. This allows both to assume something fails
 // and something succeeds.
 //
