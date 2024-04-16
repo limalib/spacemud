86d4795a64ee5d418ea3f8c7f1ddac101803cd46
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-04-16 22:44:51 +0200

    Bug fixes and updated docs.

--- lib/trans/cmds/mkdir.c
+++ lib/trans/cmds/mkdir.c
@@ -1,18 +1,17 @@
 /* Do not remove the headers from this file! see /USAGE for more info. */
 
 //: COMMAND
-// USAGE:  ``mkdir < directory >``
+// USAGE:  ``mkdir <directory>``
 //
-// This command will create a directory.
-//
+// This command will create directories.
 //
 // ``mkdir /wiz/zifnab/obj``
 //
-// will create a directory named obj in /wiz/zifnab
+// will create a directory named obj in */wiz/zifnab*
 //
-// ``mkdir obj``
+// ``mkdir obj npc``
 //
-// will try and create a directory named obj in my current directory
+// will try to create a directory named *obj* and one called *npc* in my current directory.
 //
 // ..TAGS: RST
 
@@ -26,6 +25,6 @@ void main(string *arg)
       if (mkdir(d))
          outf("Ok.\n");
       else
-         outf("Failed to make directory: %s\n", arg[0]);
+         outf("Failed to make directory: %s\n", d);
    }
 }
