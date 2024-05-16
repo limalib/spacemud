644f11210bec245d454145ad358ce5004771784a
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-05-16 23:04:30 +0200

    Fixes

--- lib/daemons/event_d.c
+++ lib/daemons/event_d.c
@@ -15,12 +15,18 @@
 // and years. This is left up to you to extend should you want to.
 //
 // This daemon on purpose does not handle event like the yearly filling of the Xmas stockings or things that happen
-// rarely. They are left for the wizards to kick off. This daemon handles things that happen often enough that players
+// rarely - they are left for the wizards to kick off. This daemon handles things that happen often enough that players
 // should experience them a few times per week depending on the settings in the */include/config/time.h* file.
+//
+// Thus the main focus is on weekly scheduled events, i.e. John goes to the bank at 16 with money suitcase Wednesdays
+// and Saturdays ("00 16 2,5"), Bob goes to the cafe to buy a sandwich for lunch everyday (except Sunday) at 11:45
+// ("45 11 0-6").
 //
 // .. TAGS: RST
 
 #include <config/time.h>
+
+#define SECS_PER_DAY 86400
 
 inherit M_DAEMON_DATA;
 
@@ -67,11 +73,11 @@ varargs string time_to_weekday(int time)
    int real_day = to_int(strftime("%w", time || adjusted_time()))
 
 #ifdef WEEK_BEGINS_MONDAY
-   +DAYS_PER_WEEK-1;
+                  + DAYS_PER_WEEK - 1;
 #else
-   ;
+       ;
 #endif
-   int day_number = ((time || adjusted_time()) % 86400) / (86400 / GAME_DAYS_PER_DAY);
+   int day_number = ((time || adjusted_time()) % SECS_PER_DAY) / (SECS_PER_DAY / GAME_DAYS_PER_DAY);
    int game_day = ((GAME_DAYS_PER_DAY * real_day) + day_number) % DAYS_PER_WEEK;
    return day_names[game_day];
 }
@@ -81,15 +87,11 @@ varargs string time_to_weekday(int time)
 // The string returned will be in game time as defined in *config/time.h*.
 varargs string time_to_str(int time)
 {
-   int t = (((time || adjusted_time()) % 86400) * GAME_DAYS_PER_DAY) % 86400;
+   int t =
+       (((time ? time + (ADJUST_HOURS * 3600) : adjusted_time()) % SECS_PER_DAY) * GAME_DAYS_PER_DAY) % SECS_PER_DAY;
    int hours = t / 3600;
    int minutes = (t % 3600) / 60;
    int seconds = (t % 60);
-   /*
-  TBUG("*********");
-  TBUG("(" + GAME_DAYS_PER_DAY + "*" + real_day + ")+" + day_number);
-  TBUG("real_day: " + real_day + " day_number: " + day_number + " game_day: " + game_day);
-   */
    return sprintf("%s %2.2d:%2.2d:%2.2d", time_to_weekday(time), hours, minutes, seconds);
 }
 
@@ -101,40 +103,101 @@ varargs int str_to_time(string date)
    int hours, minutes, seconds;
    string day;
    int delta;
-   int real_day = to_int(strftime("%w", adjusted_time()));
-   int day_number = (adjusted_time() % 86400) / (86400 / GAME_DAYS_PER_DAY);
-   // gameday+(GAME DAYS PER DAY*real day)%7
+   int real_day = (to_int(strftime("%w", adjusted_time()))
+#ifdef WEEK_BEGINS_MONDAY
+                   + DAYS_PER_WEEK - 1) %
+                  DAYS_PER_WEEK;
+#else
+                  );
+#endif
+
+   int day_number = (adjusted_time() % SECS_PER_DAY) / (SECS_PER_DAY / GAME_DAYS_PER_DAY);
    int game_day = ((GAME_DAYS_PER_DAY * real_day) + day_number) % DAYS_PER_WEEK;
    int total;
    int skip_days;
 
    sscanf(date, "%s %d:%d:%d", day, hours, minutes, seconds);
    delta = ((hours * 3600) + (minutes * 60) + seconds) / GAME_DAYS_PER_DAY;
-   /*
-      TBUG("*********");
-      TBUG("D: " + day + " H: " + hours + " min: " + minutes + " sec: " + seconds + " Delta: " + delta);
-      TBUG("(" + GAME_DAYS_PER_DAY + "*" + real_day + ")+" + day_number);
-      TBUG("real_day: " + real_day + " day_number: " + day_number + " game_day: " + game_day);
-      */
-   skip_days = member_array(day, day_names) - 1;
-   if (skip_days < game_day)
-      skip_days += sizeof(day_names);
-   total =
-       (adjusted_time() - (adjusted_time() % (86400 / GAME_DAYS_PER_DAY))) + delta + ((86400 / GAME_DAYS_PER_DAY) * skip_days);
-   /*
-     TBUG("Current day: " + game_day + " Must goto " + skip_days);
-     TBUG("Day number: " + member_array(day, day_names));
-     TBUG("Matching day: " + 14400 * skip_days);
-     TBUG(time_to_str(adjusted_time() - (adjusted_time() % 14400)));
-     TBUG("*****" + (adjusted_time() - (adjusted_time() % 14400)));
-     TBUG(delta);
-     TBUG(((86400 / GAME_DAYS_PER_DAY) * skip_days) + "******");
-     TBUG(total);
-     */
-   return total;
+
+   // Might end up negative, but that's handled below.
+   skip_days = member_array(day, day_names) - game_day;
+
+   // Calculate the total
+   total = (adjusted_time() - (adjusted_time() % (SECS_PER_DAY / GAME_DAYS_PER_DAY))) + delta +
+           ((SECS_PER_DAY / GAME_DAYS_PER_DAY) * skip_days);
+
+      TBUG("skip_days: " + skip_days + " game_day: " + game_day + " real_day: " + real_day);
+
+   // This part adds a week if we're planning for things in the past.
+   if (total - adjusted_time() < 0)
+      total += DAYS_PER_WEEK * (SECS_PER_DAY/GAME_DAYS_PER_DAY);
+
+   // Subtract adjustment to total before returning real timestamp.
+   return total - (ADJUST_HOURS * 3600);
+}
+
+int *range(string s, string type)
+{
+   int *r = ({});
+   int max;
+   if (strsrch(s, "-") != -1)
+   {
+      int *range = map(explode(s, "-"), ( : to_int($1) :));
+      for (int i = range[0]; i <= range[1]; i++)
+         r += ({i});
+   }
+   else if (strsrch(s, ",") != -1)
+      r = map(explode(s, ","), ( : to_int($1) :));
+   else if (s == "*")
+   {
+      switch (type)
+      {
+      case "minute":
+         max = 60;
+         break;
+      case "hour":
+         max = 24;
+         break;
+      default:
+         max = DAYS_PER_WEEK;
+         break;
+      }
+      for (int i = 0; i < max; i++)
+         r += ({i});
+   }
+   else
+      r = ({to_int(s)});
+   return r;
+}
+
+varargs void schedule_event(string timing, mixed ob, string extra)
+{
+   string *entries = ({});
+   string minutes, hours, days;
+   int *m_ar, *h_ar, *d_ar;
+   sscanf(timing, "%s %s %s", minutes, hours, days);
+
+   m_ar = range(minutes, "minute");
+   h_ar = range(hours, "hour");
+   d_ar = range(days, "day");
+
+   foreach (int d in d_ar)
+      foreach (int h in h_ar)
+         foreach (int m in m_ar)
+         {
+            string s = sprintf("%s %2.2d:%2.2d:00", week_day(d), h, m);
+            TBUG(s);
+            TBUG("Will run in " + time_to_string(str_to_time(s) - time()));
+            // STATE_D->add_to_queue_at_time(ob, str_to_time(s) - (3600 * ADJUST_HOURS), extra);
+         }
 }
 
 void create()
 {
    day_names = explode(DAY_NAMES, ",");
 }
+
+/*
+@EVENT_D->schedule_event("0,20,40 6,12,18 0,2,4")
+
+*/
