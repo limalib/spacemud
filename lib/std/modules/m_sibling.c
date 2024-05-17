475f400596bf15299c3d593f2f535c8f5597d788
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-05-17 13:56:40 +0200

    fix

--- lib/std/modules/m_sibling.c
+++ lib/std/modules/m_sibling.c
@@ -54,7 +54,9 @@ void set_sibling_room(mixed room)
    cached_sibling = 0;
 }
 
-private
+//: FUNCTION respond_to_sibling_ident
+// For the siblings to be able to find each other, this public function returns
+// true if the ident matches.
 int respond_to_sibling_ident(string id)
 {
    return our_ident == id;
@@ -72,6 +74,7 @@ object get_sibling()
    if (!cached_sibling)
    {
       object ob;
+      TBUG(evaluate(sibling_room));
       if (!sibling_room || !evaluate(sibling_room))
          return 0;
       ob = load_object(evaluate(sibling_room));
