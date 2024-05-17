8a11991be1b078d36b5b39556fb90e62bda04b14
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-05-17 12:19:59 +0200

    Added 'see' reference.

--- lib/cmds/wiz/netstat.c
+++ lib/cmds/wiz/netstat.c
@@ -1,6 +1,7 @@
 /* Do not remove the headers from this file! see /USAGE for more info. */
 
 //: COMMAND
+//$$ see: socketinfo
 // USAGE: ``netstat``
 //
 // Displays a list of sockets with (stat) info on each one.
