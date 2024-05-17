0fba533d6e6a25cdba5ca5b85f0c9a3cc604c5bc
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-05-17 11:12:32 +0200

    New tail.

--- lib/trans/cmds/tail.c
+++ lib/trans/cmds/tail.c
@@ -1,7 +1,7 @@
 /* Do not remove the headers from this file! see /USAGE for more info. */
 
 //: COMMAND
-// USAGE: 
+// USAGE:
 //
 //     |  ``tail <file(s)>``
 //     |  ``tail -n<number> <files>``
@@ -13,14 +13,6 @@
 
 #include <mudlib.h>
 inherit CMD;
-
-string tail(string file_contents, int n)
-{
-   if (!file_contents)
-      out("ERROR: file too large.\n");
-   else
-      out(implode(explode(file_contents, "\n")[ < n..], "\n"));
-}
 
 private
 void main(mixed *arg, mapping flags, string stdin)
@@ -37,6 +29,6 @@ void main(mixed *arg, mapping flags, string stdin)
    }
    for (i = 0; i < sizeof(arg[0]); i++)
    {
-      tail(read_file(arg[0][i]), n);
+      more(tail(arg[0][i], n));
    }
 }
