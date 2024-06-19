c578be60826249fd62e556bba83240e0b761d1e1
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-06-19 21:04:21 +0200

    Bit better interface for 'quiet' command.

--- lib/cmds/wiz/quiet.c
+++ lib/cmds/wiz/quiet.c
@@ -18,6 +18,21 @@ void main(mixed *arg)
    object ob;
 
    ob = arg[0];
+   
+   //Doesn't work on people
+   if (ob->is_body())
+   {
+      outf("Making %s quiet is beyond your powers.", ob->short());
+      return;
+   }
+
+   //May not have actions.
+   if (!arrayp(ob->query_actions()))
+   {
+      outf("%s is already quiet.", ob->short());
+      return;
+
+   }
    ob->stop_actions();
    ob->set_actions(100, 0);
    outf("%s has been quieted.", ob->short());
