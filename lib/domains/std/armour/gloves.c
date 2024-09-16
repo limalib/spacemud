fe321cf67eb1ee6a2c728365768634abc5f2f6cf
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-09-16 12:11:27 +0200

    Removed AC from gloves

--- lib/domains/std/armour/gloves.c
+++ lib/domains/std/armour/gloves.c
@@ -5,7 +5,6 @@ inherit ARMOUR;
 void setup()
 {
    set_adj("pair of");
-   set_armour_class(3);
    set_id("gloves");
    set_long("These are black gloves made of fine leather. Perhaps.");
    set_slot("left hand");
