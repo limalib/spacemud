69565b72525ba91ccb0143c80f6ccb4b93fd2275
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-09-25 18:06:22 +0200

    lower_case() missing

--- lib/domains/std/pet.c
+++ lib/domains/std/pet.c
@@ -101,7 +101,8 @@ void setup()
    string name;
 
    master = this_body();
-   if (file = read_file(WIZ_DIR+"/" + master->query_name() + "/.petrc"))
+
+   if (file = read_file(WIZ_DIR+"/" + lower_case(master->query_name()) + "/.petrc"))
       m = parse_file(file);
    else
       m = ([]);
