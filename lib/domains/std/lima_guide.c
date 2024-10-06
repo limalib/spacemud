754cad1d9571c135e3e00bd775774e1b372b6649
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-10-06 13:13:08 +0200

    New docs URL

--- lib/domains/std/lima_guide.c
+++ lib/domains/std/lima_guide.c
@@ -12,7 +12,7 @@ void setup()
             "single word, that appears to be pulsing with a life of its own, LIMA.");
    set_text("This book will at some point contain some interesting bits of information about the LIMA mudlib.  However "
             "at this time, much like the documentation of the lib itself, it is unfinished.");
-   set_entries((["1":"Do read https://limamudlib.readthedocs.io/",
+   set_entries((["1":"Do read https://docs.limalib.dev/",
                  "2":"See https://github.com/limalib/lima"]));
    set_synonyms((["docs":"1", "source":"2"]));
 }
