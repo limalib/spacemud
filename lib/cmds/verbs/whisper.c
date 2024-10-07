667e4d467075a9980bdf0e260969e3e532831a15
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-10-07 11:03:00 +0200

    Handle USE_INTRODUCTIONS for whisper.

--- lib/cmds/verbs/whisper.c
+++ lib/cmds/verbs/whisper.c
@@ -20,13 +20,47 @@ void whisper_it(object liv, string str)
 {
    string *msgs;
    object *who = ({this_body(), liv});
+   object *others;
+#ifdef USE_INTRODUCTIONS
+   string name, name2;
 
-   str = punctuate(str);
+   others = all_inventory(environment(this_body())) - ({this_body(), liv});
 
+   // Handle whisper names for others in the room.
+   foreach (object player in others)
+   {
+      string msg;
+
+      if (player->is_introduced(this_body()))
+         name = this_body()->query_name();
+      else
+      {
+         name = this_body()->physical_appearance();
+         if (wizardp(player))
+            name = "[" + this_body()->query_name() + "] " + name;
+      }
+
+      if (player->is_introduced(liv))
+         name2 = liv->query_name();
+      else
+      {
+         name2 = lower_case(liv->physical_appearance());
+         if (wizardp(player))
+            name2 = "[" + liv->query_name() + "] " + name2;
+      }
+
+      // Make sure wizards can see who is talking.
+      msg = name + " whispers something to " + punctuate(name2) + "<res>";
+      tell(player, msg);
+   }
+   tell(this_body(), "You whisper to " + liv->query_name() + ": " + punctuate(str) + "<res>");
+   tell(liv, this_body()->query_name() + " whispers to you: " + punctuate(str) + "<res>");
+#else
    msgs = action(who, "$N $vwhisper to $t: $o", str);
    // change other message
    msgs[2] = this_body()->query_name() + " whispers something to " + liv->query_name() + ".\n";
    inform(who, msgs, environment(this_body()));
+#endif
 }
 
 void do_whisper_liv_str(object liv, string str)
