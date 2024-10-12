bdc87cf7a021edc029a85b47b2ee6cfc18c5bde3
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-10-12 07:22:35 +0200

    Fix for a missing rank (Asmerian)

--- lib/daemons/skill_d.c
+++ lib/daemons/skill_d.c
@@ -32,7 +32,7 @@ inherit CLASS_SKILL;
 private
 mapping skills = ([]);
 private
-nosave mixed skill_ranks = ({100,  250,  500,  750,  1000, 1250, 1500, 2000, 2500, 3000,
+nosave mixed skill_ranks = ({100,  250,  500,  750,  1000, 1250, 1500, 1750, 2000, 2500, 3000,
                              3500, 4000, 4500, 5000, 5500, 6000, 6500, 7000, 8000, 10000});
 #ifdef USE_ROMAN_NUMERALS
 private
