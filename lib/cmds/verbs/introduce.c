718ce950ad3273d549fef0656ffd191f2bbf5d4f
Tsathoggua of NKai <sstaunp+github@gmail.com>
2024-10-25 23:25:41 +0200

    Extended and bug fixed  #20

--- lib/cmds/verbs/introduce.c
+++ lib/cmds/verbs/introduce.c
@@ -2,23 +2,41 @@
 
 inherit VERB_OB;
 
-void do_introduce_obj(object ob)
-{
-   this_body()->simple_action("$N $vare happy to introduce: $O!", ob);
-}
-
 void do_introduce()
 {
    write("Introduce who?");
+}
+
+void do_introduce_wrd_to_obj(string word, object ob)
+{
+   if (word != "yourself" && word != "myself")
+   {
+      write("You can only introduce yourself.");
+      return;
+   }
+
+   if (!ob->is_body())
+   {
+      write("You fail to introduce yourself to " + ob->short() + ".");
+      return;
+   }
+
+   tell(this_body(), "You introduce yourself to " + ob->query_name() + ".");
+   tell(ob, this_body()->query_name() + " introduces " + this_body()->query_reflexive() + " to you.");
+}
+
+void do_introduce_to_obj(object ob)
+{
+   return do_introduce_wrd_to_obj("yourself", ob);
 }
 
 void do_introduce_wrd(string word)
 {
    object *players = filter_array(all_inventory(environment(this_body())), ( : $1->is_body() && $1 != this_body() :));
 
-   if (word != "yourself")
+   if (word != "yourself" && word != "myself")
    {
-      write("Introduce who?");
+      write("You can only introduce yourself.");
       return;
    }
 
@@ -32,11 +50,23 @@ void do_introduce_wrd(string word)
                              this_body()->query_name());
 }
 
+void do_introduce_obj(object ob)
+{
+   if (ob == this_body())
+      return do_introduce_wrd("yourself");
+   if (ob->is_body())
+      this_body()->simple_action("$N $vare happy to introduce: $O!", ob->query_name());
+   else
+      write("You are not sure how to introduce that.");
+}
+
 void create()
 {
    add_rules(({
-       "",
        "WRD",
+       "WRD to OBJ",
+       "to OBJ",
        "OBJ",
+       "",
    }));
 }
